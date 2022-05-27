c5c2dfe4649dd430459c63c3e1b880c7998e3452
danghoanggeo <dang.hoang.geo@gmail.com>
2022-05-27 18:12:59 +0900

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Dang Hoang on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
